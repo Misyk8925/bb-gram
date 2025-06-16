0cbec36d94d295f352eb5fd30224afa6ebe8ec02
Misyk8925 <129992573+Misyk8925@users.noreply.github.com>
2025-06-16 08:12:28 +0200

    refactor catchEverythingFilter to improve error response handling for 500 status

--- src/common/filters/catch.everything.filter.ts
+++ src/common/filters/catch.everything.filter.ts
@@ -17,20 +17,37 @@ export class CatchEverythingFilter implements ExceptionFilter {
             exception instanceof HttpException
                 ? exception.getStatus()
                 : 500
-        const responseBody = {
-            statusCode: httpStatus,
-            timestamp: new Date().toISOString(),
-            path: httpAdapter.getRequestUrl(ctx.getRequest()),
-            message: exception instanceof HttpException
-                ? exception.message
-                : exception instanceof Error
-                    ? exception.message
-                    : String(exception)
-        };
+        let responseBody: any
+        if (httpStatus === 500) {
 
-        httpAdapter.reply(
-            ctx.getResponse(), responseBody, httpStatus
-        );
+            responseBody = {
+                statusCode: httpStatus,
+                timestamp: new Date().toISOString(),
+            }
+            httpAdapter.reply(
+                ctx.getResponse(),
+                responseBody,
+                httpStatus
+            )
+        }
+
+        else {
+            responseBody = {
+                statusCode: httpStatus,
+                timestamp: new Date().toISOString(),
+                path: httpAdapter.getRequestUrl(ctx.getRequest()),
+                message: exception instanceof HttpException
+                    ? exception.message
+                    : exception instanceof Error
+                        ? exception.message
+                        : String(exception)
+            };
+
+            httpAdapter.reply(
+                ctx.getResponse(), responseBody, httpStatus
+            );
+        }
+
         this.logger.error(responseBody);
     }
 
